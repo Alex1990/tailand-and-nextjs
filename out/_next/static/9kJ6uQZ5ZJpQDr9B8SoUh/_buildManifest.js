53142a6f9a9c84c59823b458741abcdabee77047
Alex Chao <alexchao1990@gmail.com>
2021-02-20 12:52:21 +0800

    Export

---
+++ out/_next/static/9kJ6uQZ5ZJpQDr9B8SoUh/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-e7788c053fa4c1ddfd44.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-89f93b96e903ba7827c9.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
